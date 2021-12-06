01396de04a110f3a50caa8efbf45412bf34eca35
Vladislav Snezhkin <snezhkinv@yandex-team.ru>
2021-12-06 21:29:38 +0300

    первое занятие: Модуль новости создание, получени, удаление

---
+++ src/news/news.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Param, Post, Body, Delete } from '@nestjs/common';
+import { News, NewsService } from './news.service';
+
+@Controller('news')
+export class NewsController {
+  constructor(private readonly newsService: NewsService) {}
+
+  @Get('/:id')
+  get(@Param('id') id: string): News {
+    const idInt = parseInt(id);
+    return this.newsService.find(idInt);
+  }
+
+  @Post()
+  create(@Body() news: News): News {
+    return this.newsService.create(news);
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') id: string): string {
+    const idInt = parseInt(id);
+    const isRemoved = this.newsService.remove(idInt);
+    return isRemoved ? 'Новость удалена' : 'Передан неверный идентификатор';
+  }
+}
